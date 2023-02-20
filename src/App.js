7ef63169634e13ad08d6790894d26e5399694192
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-02-20 21:09:48 +0530

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Header } from './components'
+import { Route, Routes } from 'react-router-dom'
+import { CreateContainer, Header, MainContainer } from './components'
 
 const App = () => {
   return (
     <div className='w-screen h-auto flex bg-primary flex-col'>
       <Header />
-      <main className='mt-24 p-8 w-full'>Main</main>
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path="/" element={<MainContainer />} />
+          <Route path="/createItem" element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   )
 }
