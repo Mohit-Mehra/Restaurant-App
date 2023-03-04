e854bb671d10f6116668ab4a2cc79b209c84ee31
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-03-04 21:40:30 +0530

    main container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -109,7 +109,13 @@ const Header = () => {
       </div>
       {/* mobile */}
       <div className="flex items-center justify-between md:hidden w-full h-full">
-        <Link to={"/"} className="flex items-center gap-2">
+        <div className="flex items-center justify-center relative">
+            <MdShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
+          </div>
+          <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="Logo" />
           <p className="text-headingColor text-xl font-bold">City</p>
         </Link>
